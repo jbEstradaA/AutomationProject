af05ad35739f135d4916bc4e5b5487c7af0dd5e2
Shaedox <vhph_20@hotmail.com>
2019-10-14 22:59:18 -0500

    Adding test scenarios to cover subject heading control functionality

--- src/test/java/com/automationPractice/pages/ContactUsPage.java
+++ src/test/java/com/automationPractice/pages/ContactUsPage.java
@@ -11,13 +11,47 @@ public class ContactUsPage extends ParentPage {
 
     By Form_principal = By.id("center_column");
     By Tittle_ContactUsForm = By.xpath("//div[@id='center_column']//h1");
+    By Select_subject = By.xpath("//select[@id='id_contact']");
+    By Span_subject = By.xpath("//div[@id='uniform-id_contact']/span");
+    By Desc_subject2 = By.id("desc_contact2");
+    By Desc_subject1 = By.id("desc_contact1");
 
     /**
-     * This Method is to verify if the principal form is Displayed
+     * This method is to verify if the principal form is Displayed
      */
     public Boolean verifyThatPrincipalFormIsDisplayed(){
         return verifyIfElementIsDisplayed(Form_principal);
     }
 
+    /**
+     * This method is to do click on the SelectControl of subject heading
+     */
+    public void doClickOnSubjectSelectControl(){
+        clickOnElement(Select_subject);
+    }
 
+    /**
+     * This method is used to select a determined subject of the SelectControl
+     * @param Subject is the Subject type, can be "Customer service" or "Webmaster"
+     */
+    public void selectSubjectType(String Subject) {
+        selectItemOfDrpElement(Select_subject, Subject);
+    }
+
+    /**
+     * This method is used to get the text value of the Web element that contains the description of the selected subject
+     * @return a description text value
+     */
+    public String getSubjectDescription() {
+        String TextValue = getTextFromElement(Span_subject);
+        switch (TextValue){
+            case "Customer service":
+                TextValue = getTextFromElement(Desc_subject2);
+                break;
+            case "Webmaster":
+                TextValue = getTextFromElement(Desc_subject1);
+                break;
+        }
+        return TextValue;
+    }
 }
--- src/test/resources/features/automationPractice/ContactUs.feature
+++ src/test/resources/features/automationPractice/ContactUs.feature
@@ -5,7 +5,14 @@ Feature: Contact Us
     When do click in the contact us button
     Then the CUSTOMER SERVICE from must be displayed
 
-  Scenario: new scenario for new myFirstBranch
-    Given 1111111
-    When 22222222
-    Then 3333333
+  Scenario:	Verify that a description message is displayed when the user select the subject type CUSTOMER SERVICE
+    Given The user is in the CONTACT US section
+    When The user click on subject heading control
+    And The user select the subject type "Customer service"
+    Then Verify that description message "For any question about a product, an order" must be displayed
+
+  Scenario: Verify that a description message is displayed when the user select the subject type WEBMASTER
+    Given The user is in the CONTACT US section
+    When The user click on subject heading control
+    And The user select the subject type "Webmaster"
+    Then Verify that description message "If a technical problem occurs on this website" must be displayed
